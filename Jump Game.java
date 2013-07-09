62a9e4c039c86e5bf9ff22556a95bedbc03a40a7
David <donadona0929@gmail.com>
2013-07-09 05:24:28 -0700

    Update Jump Game.java

--- Jump Game.java
+++ Jump Game.java
@@ -7,7 +7,6 @@ A = [2,3,1,1,4], return true.
 A = [3,2,1,0,4], return false.
 */
 
-
 public class Solution {
     public boolean canJump(int[] A) {
         // Start typing your Java solution below
