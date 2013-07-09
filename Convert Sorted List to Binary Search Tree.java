363bac434e6fcd3fe8c008d0d0d52c380e7a2bd3
David <donadona0929@gmail.com>
2013-07-09 05:57:44 -0700

    Create Convert Sorted List to Binary Search Tree.java

---
+++ Convert Sorted List to Binary Search Tree.java
@@ -0,0 +1,40 @@
+/*
+Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
+*/
+
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+ 
+public class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if (head == null) return null;
+        ListNode slow = head, fast = head, prev = null;
+       
+        fast = fast.next;
+       
+        while (fast != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next;
+            if (fast != null) fast = fast.next;
+        }
+
+        TreeNode root = new TreeNode(slow.val);
+        if (prev != null) {
+            prev.next = null;
+            root.left = sortedListToBST(head);
+        }
+       
+        root.right = sortedListToBST(slow.next);
+        return root;
+    }
+}
